f1731a18696306ee90d945d081a56e342e506a0c
ashikmv180 <70476302+ashikmv180@users.noreply.github.com>
2020-09-09 23:47:32 +0530

    Add files via upload
    i have uploaded the first assignment

---
+++ assignment1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(2000,3201):
+    
+    if(i%7==0 and i%5!=0):
+        print(i,end=",")
+
+
+# In[2]:
+
+
+a=input("enter the first name:")
+b=input("enter the last name:")
+print(b, a)
+
+
+# In[3]:
+
+
+import math
+d=12
+r=d/2
+a=(4/3)*math.pi*r*r*r
+
+
+# In[4]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+
+
